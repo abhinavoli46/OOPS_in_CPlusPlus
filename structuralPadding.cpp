3702d093c8411655cee6fe68cdd06e17c6ddee9c
Abhinav Oli <oliabhinav606@gmail.com>
2023-08-22 19:53:10 +0530

    Structural Padding

---
+++ structuralPadding.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct s{
+    double b;
+    int a;
+    double c;
+};
+int main()
+{
+    char arr[1] = {'b'};
+    cout << atoi(arr);
+    return 0;
+}
